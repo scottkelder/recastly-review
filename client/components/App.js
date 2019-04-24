1d21aeb04d105f967c5112befa0641877ab8a94a
Scott Elder <smelder@gmail.com>
2019-04-23 21:20:28 -0500

    Tweaked for readability

--- client/components/App.js
+++ client/components/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import Search from './Search.js';
+import VideoList from './VideoList';
+import VideoPlayer from './VideoPlayer';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      videoList: null,
-      currentVideo: null
+      videoList: [
+        {
+          title: 'title1',
+          description: 'description',
+          image: 'https://i.imgur.com/LulPH2z.jpg'
+        },
+        {
+          title: 'title2',
+          description: 'description2',
+          image: 'https://i.imgur.com/JbLp4mO.jpg'
+
+        },
+        {
+          title: 'title3',
+          description: 'description3',
+          image: 'https://i.imgur.com/t2Q3S0u.jpg'
+        }
+      ],
+      currentVideo: {
+        title: 'title',
+        description: 'description',
+        image: 'https://i.imgur.com/LulPH2z.jpg'
+      }
     }
   }
   entryClick(target) {
--- client/components/VideoPlayer.js
+++ client/components/VideoPlayer.js
@@ -4,9 +4,10 @@ const VideoPlayer = (props) => {
   const defaultVideo = props.videos[0];
   return (
     <div>
-      <p>{props.video.title}</p>
+      <h1>{props.video.title}</h1>
       <p>{props.video.desciption}</p>
-      <iframe url={props.video.url}></iframe>
+      <img src={props.video.image} />
+      {/* <iframe url={props.video.url}></iframe> */} {/* <---- That is how it should be (an iframe.) Becasue we are not querying YouTube, I put an image tag so you would know it works. */}
     </div>
   )
 }
