35370c539b22a4fc1ecf2282608e6ab4f475f79d
Scott Elder <smelder@gmail.com>
2019-04-23 21:40:06 -0500

    Removed unused variable

--- client/components/VideoPlayer.js
+++ client/components/VideoPlayer.js
@@ -1,13 +1,12 @@
 import React, {Fragment} from 'react'
 
 const VideoPlayer = (props) => {
-  const defaultVideo = props.videos[0];
   return (
     <div>
       <h1>{props.video.title}</h1>
       <p>{props.video.desciption}</p>
       <img src={props.video.image} />
-      {/* <iframe url={props.video.url}></iframe> */} {/* <---- That is how it should be (an iframe.) Becasue we are not querying YouTube, I put an image tag so you would know it works. */}
+      {/* <iframe url={props.video.url}></iframe> */} {/* <---- That is how it should be (an iframe.) Becasue we are not actually querying YouTube, I put an image tag so you would know it works. */}
     </div>
   )
 }
